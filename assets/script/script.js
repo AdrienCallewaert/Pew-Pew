96922252ae3120b27b250b47b23b84e48e643af8
Adrien Callewaert <76485285+AdrienCallewaert@users.noreply.github.com>
2021-03-03 15:25:36 +0100

    Update script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -19,7 +19,10 @@ class Player{
 	}
 }
 
-const player = new Player(100, 100, 30, '#138D75')
+const x = canvas.width / 2
+const y = canvas.height / 2
+
+const player = new Player(x, y, 30, '#138D75')
 
 player.draw()
 
